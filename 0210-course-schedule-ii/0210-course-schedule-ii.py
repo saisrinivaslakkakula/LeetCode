793df972359143807c20d14a69b971226246478c
saisrinivasl <38246768+saisrinivaslakkakula@users.noreply.github.com>
2023-10-24 03:58:40 -0400

    Time: 90 ms (94.53%), Space: 17.9 MB (76.59%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,30 +1,34 @@
 from collections import defaultdict
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        if len(prerequisites) == 0:
-            return [i for i in range(numCourses)]
+        if numCourses == 0: return [i for i in range(numCourses)]
         graph = defaultdict(list)
-        for dest,src in prerequisites:
-            graph[src].append(dest)
-        
-        indegree = [0]*numCourses
+        for dst,src in prerequisites:
+            graph[src].append(dst)
+        in_degree = [0]*numCourses
 
-        for neighbors in graph.values():
-            for neighbor in neighbors:
-                indegree[neighbor]+=1
+        for neighbours in graph.values():
+            for n in neighbours:
+                in_degree[n]+=1
+        
         q = deque()
-        for i,ind in enumerate(indegree):
-            if ind == 0:
-                q.append(i)
+        for node_num,id_ in enumerate(in_degree):
+            if id_ == 0:
+                q.append(node_num)
+        
         ans = []
         while q:
             cur = q.popleft()
             ans.append(cur)
             for n in graph[cur]:
-                if indegree[n] !=0 :
-                    indegree[n] -=1
-                if indegree[n] == 0:
+                if in_degree[n] !=0:
+                    in_degree[n] -= 1
+                if in_degree[n] == 0:
                     q.append(n)
         
         if len(ans) == numCourses: return ans
         else: return []
+                
+
+        
+        
