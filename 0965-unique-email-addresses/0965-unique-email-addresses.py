119a4b6c927c44f7b43f45a54de2d1a2f92b4c35
saisrinivasl <38246768+saisrinivaslakkakula@users.noreply.github.com>
2023-10-24 04:24:36 -0400

    Time: 60 ms (60.17%), Space: 16.3 MB (90.04%) - LeetHub

--- 0965-unique-email-addresses/0965-unique-email-addresses.py
+++ 0965-unique-email-addresses/0965-unique-email-addresses.py
@@ -1,13 +1,14 @@
 class Solution:
     def numUniqueEmails(self, emails: List[str]) -> int:
-        unique = set()
+        res = set()
         for email in emails:
             local,domain = email.split('@')
             if '+' in local:
                 local = local.split('+')[0]
             if '.' in local:
-                local = ''.join(local.split('.'))
-            final_email = local + "@" + domain
-            unique.add(final_email)
-        return(len(unique))
+                local = local.replace('.','')
+            res.add(local+'@'+domain)
+        #print(res)
+        return(len(res))
+
         
